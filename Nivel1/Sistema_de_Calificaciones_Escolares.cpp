b9431e7baa388119270a67b8e3cef75377d36966
Monika <al23760057@ite.edu.mx>
2026-09-06 19:50:53 -0700

    NIVEL 1
     Estructura básica y declaración de variables

--- Nivel1/Sistema_de_Calificaciones_Escolares.cpp
+++ Nivel1/Sistema_de_Calificaciones_Escolares.cpp
@@ -1,1 +1,22 @@
+`#include <iostream>
+int main();
 
+string nombre;
+int edad;
+float calificacion1, calificacion2, calificacion3;
+float promedio;
+
+cin<<"Introduce el nombre">>
+  cin<<"Introduce la edad">>
+  cin<<"Introduce la calificación 1">>
+    cin<<"Introduce la calificación 2">>
+    cin<<"Introduce la calificación 3">>
+
+  promedio = calificacion1 + calificacion2 + calificacion3 /3
+
+  cout<<"Tu nombre es: ">>
+    cout<<"Tu edad es: ">>
+  cout<<"Tu promedio es: ">>
+
+  return 0;
+  
