5b6f5f162ece67764d46f286275a05c30d44c7f0
Monika <al23760057@ite.edu.mx>
2026-09-15 11:06:06 -0700

    Nivel 4
    NIVEL 4 — Ciclo for

--- Nivel1/Sistema_de_Calificaciones_Escolares.cpp
+++ Nivel1/Sistema_de_Calificaciones_Escolares.cpp
@@ -71,6 +71,162 @@ if promedio >= 9;
 }
 
 
+_____________________________________________________________
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int opcion;
+
+    do {
+        cout << "\n===== SISTEMA DE CALIFICACIONES =====" << endl;
+        cout << "1. Registrar estudiante" << endl;
+        cout << "2. Ver informacion del programa" << endl;
+        cout << "3. Salir" << endl;
+        cout << "Opcion: ";
+        cin >> opcion;
+
+        switch (opcion) {
+
+            case 1: {
+                string nombre;
+                int edad;
+                int n;
+
+                float calificacion;
+                float suma = 0;
+                float promedio;
+                float calificacionAlta;
+                float calificacionBaja;
+
+                int aprobadas = 0;
+                int reprobadas = 0;
+
+                cout << "\n--- Registrar estudiante ---" << endl;
+
+                cout << "Introduce el nombre: ";
+                cin >> nombre;
+
+                cout << "Introduce la edad: ";
+                cin >> edad;
+
+                // Validar edad
+                if (edad < 0 || edad > 120) {
+                    cout << "Edad invalida." << endl;
+                    break;
+                }
+
+                cout << "Cuantas calificaciones deseas registrar? ";
+                cin >> n;
+
+                if (n <= 0) {
+                    cout << "La cantidad de calificaciones debe ser mayor que 0."
+                         << endl;
+                    break;
+                }
+
+              
+                cout << "Introduce la calificacion 1: ";
+                cin >> calificacion;
+
+                suma = calificacion;
+
+                calificacionAlta = calificacion;
+                calificacionBaja = calificacion;
+
+                if (calificacion >= 6) {
+                    aprobadas++;
+                } else {
+                    reprobadas++;
+                }
+
+                
+                for (int i = 2; i <= n; i++) {
+
+                    cout << "Introduce la calificacion " << i << ": ";
+                    cin >> calificacion;
+
+                    suma += calificacion;
+
+                    
+                    if (calificacion >= 6) {
+                        aprobadas++;
+                    } else {
+                        reprobadas++;
+                    }
+
+                    if (calificacion > calificacionAlta) {
+                        calificacionAlta = calificacion;
+                    }
+
+                    if (calificacion < calificacionBaja) {
+                        calificacionBaja = calificacion;
+                    }
+                }
+
+               
+                promedio = suma / n;
+
+             
+                cout << "\n===== Menú =====" << endl;
+                cout << "Tu nombre es: " << nombre << endl;
+                cout << "Tu edad es: " << edad << endl;
+                cout << "Tu promedio es: " << promedio << endl;
+
+                cout << "Calificacion mas alta: "
+                     << calificacionAlta << endl;
+
+                cout << "Calificacion mas baja: "
+                     << calificacionBaja << endl;
+
+                cout << "Calificaciones aprobatorias: "
+                     << aprobadas << endl;
+
+                cout << "Calificaciones reprobatorias: "
+                     << reprobadas << endl;
+
+               
+                if (promedio >= 9) {
+                    cout << "Excelente" << endl;
+                }
+                else if (promedio >= 7) {
+                    cout << "Aprobado" << endl;
+                }
+                else if (promedio >= 6) {
+                    cout << "Regular" << endl;
+                }
+                else {
+                    cout << "Reprobado" << endl;
+                }
+
+                break;
+            }
+
+            case 2:
+                cout << "\n--- Menu del programa ---" << endl;
+                cout << "Sistema de Calificaciones" << endl;
+                cout << "Registro del estudiante y "
+                     << "calcular su promedio." << endl;
+                cout << "Muestra la calificacion mas alta, "
+                     << "la mas baja, aprobadas y reprobadas." << endl;
+                break;
+
+            case 3:
+                cout << "\nSalir" << endl;
+                break;
+
+            default:
+                cout << "\nOpcion no valida." << endl;
+                break;
+        }
+
+    } while (opcion != 3);
+
+    return 0;
+}
+
 
 
 
